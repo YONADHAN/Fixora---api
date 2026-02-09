c07116f0e3d1dd122426754ba3a02fd81a6661a6
Yonadhan <yonadhanmm0@gmail.com>
2026-02-09 21:25:27 +0530

    .

--- src/presentation/socket/socket.server.ts
+++ src/presentation/socket/socket.server.ts
@@ -1,44 +1,3 @@
-// import { Server } from 'socket.io'
-// import http from 'http'
-// import chalk from 'chalk'
-
-// import { socketAuthMiddleware } from './socket.auth'
-// import { registerRooms } from './socket.room'
-// import { registerSocketEvents } from './socket.events'
-
-// let io: Server
-
-// export const initSocketServer = (httpServer: http.Server) => {
-//   io = new Server(httpServer, {
-//     cors: {
-//       origin: process.env.NEXT_FRONTEND_URL,
-//       credentials: true,
-//     },
-//   })
-
-//   io.use(socketAuthMiddleware)
-
-//   io.on('connection', (socket) => {
-//     registerRooms(socket)
-//     registerSocketEvents(socket)
-
-//     console.log(
-//       chalk.cyanBright('🔌 Socket connected:'),
-//       chalk.white(socket.id)
-//     )
-//   })
-
-//   console.log(
-//     chalk.greenBright.bold('⚡ Socket.IO server initialized and ready')
-//   )
-
-//   return io
-// }
-
-// export const getIO = () => {
-//   if (!io) throw new Error('Socket.io not initialized')
-//   return io
-// }
 import { Server } from 'socket.io'
 import http from 'http'
 import chalk from 'chalk'
@@ -78,7 +37,6 @@ export const initSocketServer = (httpServer: http.Server) => {
           userId: user.userId,
         })
       }
-
 
       onlineUsers.forEach((_, id) => {
         if (id !== user.userId) {
