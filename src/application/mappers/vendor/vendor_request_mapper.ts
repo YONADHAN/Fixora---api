e2986157213f33c04de1c867460868e744a40d60
Yonadhan <yonadhanmm77@gmail.com>
2026-03-18 20:16:38 +0530

    refactor(type): removal of any

--- src/application/mappers/vendor/vendor_request_mapper.ts
+++ src/application/mappers/vendor/vendor_request_mapper.ts
@@ -1,13 +1,14 @@
+import { IVendorEntity } from '../../../domain/models/vendor_entity'
 import { VendorRequestDTO } from '../../dtos/vendor_dto'
 
 export class VendorRequestMapper {
-  static toDTO(vendor: any): VendorRequestDTO {
+  static toDTO(vendor: IVendorEntity): VendorRequestDTO {
     return {
-      userId: vendor.userId,
+      userId: vendor.userId || "",
       name: vendor.name,
       email: vendor.email,
       documents: Array.isArray(vendor.documents)
-        ? vendor.documents.map((doc: any) => ({
+        ? vendor.documents.map((doc) => ({
           name: doc.name,
           url: doc.url,
         }))
